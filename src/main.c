d76966f11ae2cebe7688554ca1298fc7eb57aaef
Ethan Schweinsberg <ees37@zips.uakron.edu>
2016-09-19 15:58:06 -0400

    Initial commit for sample program that toggles the output on a pin. This will be used for flashing an LED.

---
+++ src/main.c
@@ -0,0 +1,18 @@
+#include <avr/io.h>
+#include <avr/delay.h>
+
+int main(void)
+{
+	// set port B as output
+	DDRB = 0b11111111;
+
+	while(1 == 1)
+	{
+		// turn LED on 
+		PORTB = PORTB | 1;
+		// wait for 500 ms
+		_delay_ms(500);
+		// turn LED off
+		PORTB = PORTB | 1; 		
+	}
+}
